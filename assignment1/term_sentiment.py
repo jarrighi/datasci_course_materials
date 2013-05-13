03c90a1810643f40a686e7f53062fad8c265b72b
Jules Arrighi <juliana.arrighi@gmail.com>
2013-05-13 13:41:56 -0700

    First attempt at term_sentiment.py

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -1,17 +1,29 @@
 import sys
+import json
+import tweet_sentiment as t
 
-def hw():
-    print 'Hello, world!'
-
-def lines(fp):
-    print str(len(fp.readlines()))
+def print_term_sentiments(tweets, sents):
+  new_terms = {}
+  for item in tweets:
+    tweet = json.loads(item)
+    if 'text' in tweet:
+      sent = t.calc_sent(tweet['text'], sents)
+      for word in tweet['text'].split():
+        if not word in sents:
+          if word in new_terms:
+            new_terms[word][1] += 1
+            new_terms[word][0] = (new_terms[word][0] + sent)/new_terms[word][1]
+          else: 
+            new_terms[word] = [sent, 1]
+  for i in new_terms:
+    print i,
+    print new_terms[i][0]
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+  sent_file = open(sys.argv[1])
+  tweet_file = open(sys.argv[2])
+  sent_dict = t.make_sent_dict(sent_file)
+  print_term_sentiments(tweet_file, sent_dict)
 
 if __name__ == '__main__':
     main()
