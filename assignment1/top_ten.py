c4b48697cd14739981897a994a6dd071bfa19f37
Jules Arrighi <juliana.arrighi@gmail.com>
2013-05-13 20:09:56 -0700

    Updated top_ten.py to use hashtag field

--- assignment1/top_ten.py
+++ assignment1/top_ten.py
@@ -7,21 +7,16 @@ def print_top_ten_tags(tweet_file):
 
 def get_tag_counts(tweet_file):
   tag_counts = {}
-  total_count = 0
-  for item in tweet_file :
+  for item in tweet_file:
     tweet = json.loads(item)
-    if 'text' in tweet:
-      tweet_text = tweet['text']
-      for word in tweet_text.split():
-        if word[0] == '#':
-          if word in tag_counts:
-            tag_counts[word] += 1
-            total_count += 1
-          else:
-            tag_counts[word] = 1
-            total_count += 1
-  for tag in tag_counts:
-    tag_counts[tag] = float(tag_counts[tag]) / float(total_count)
+    if 'entities' in tweet:
+      tags = tweet['entities']['hashtags']
+      for tag in tags:
+        word = tag['text']
+        if word in tag_counts:
+          tag_counts[word] += 1
+        else:
+          tag_counts[word] = 1.0
   return tag_counts
 
 def print_top(n, tags):
