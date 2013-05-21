dc1ad9d14ec6945bcda0d918d9c25ea93b621eff
Jules Arrighi <juliana.arrighi@gmail.com>
2013-05-20 22:29:12 -0700

    adding unique_trims.py

---
+++ assignment3/unique_trims.py
@@ -0,0 +1,34 @@
+import MapReduce
+import sys
+
+"""
+Word Count Example in the Simple Python MapReduce Framework
+"""
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+def mapper(record):
+    # key: sequence id
+    # value: string of nucleotides
+    key = record[0]
+    value = record[1]
+    new_value = value[:-10]
+    mr.emit_intermediate(new_value, 1)
+
+def reducer(key, list_of_values):
+    # key: nucleotide string
+    # value: list of occurrence counts
+    total = 0
+    for v in list_of_values:
+      total += v
+    if v == 1:
+      mr.emit((key))
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
