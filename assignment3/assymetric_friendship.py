0fb10fc3ea679a44d9f461cdd9c4617e6c9e8e5b
Jules Arrighi <juliana.arrighi@gmail.com>
2013-05-20 22:17:11 -0700

    added assymetric_friendship.py

---
+++ assignment3/assymetric_friendship.py
@@ -0,0 +1,35 @@
+import MapReduce
+import sys
+
+"""
+Word Count Example in the Simple Python MapReduce Framework
+"""
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+def mapper(record):
+    # key: person
+    # value: person's friend
+    person = record[0]
+    friend = record[1]
+    mr.emit_intermediate(person + " " + friend, 1)
+    mr.emit_intermediate(friend + " " + person, 1)
+
+def reducer(key, list_of_values):
+    # key: word
+    # value: list of occurrence counts
+    total = 0
+    for v in list_of_values:
+      total += v
+    if total == 1:
+      output = key.split()
+      mr.emit((output[0], output[1]))
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
